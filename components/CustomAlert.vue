a7934b68ff38374f9579206df16bd516a1a24a28
RothaSAMON <samonrotha@gmail.com>
2025-01-18 17:49:44 +0700

    Added the custom Message of Server Side

---
+++ components/CustomAlert.vue
@@ -0,0 +1,41 @@
+<template>
+  <transition name="fade">
+    <a-alert
+      v-if="alertStore.isVisible"
+      class="custom-alert"
+      :type="alertStore.type"
+      :message="alertStore.message"
+      show-icon
+      :closable="true"
+      @close="alertStore.clearAlert"
+    />
+  </transition>
+</template>
+
+<script lang="ts" setup>
+import { useAlertStore } from "@/store/alertStore";
+
+const alertStore = useAlertStore();
+</script>
+
+<style scoped>
+.custom-alert {
+  position: absolute;
+  top: 1rem;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 1000;
+  max-width: 90%;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
